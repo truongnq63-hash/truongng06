f68459dcf9f14f7028a40efbac38277e67a01eca
Trường Ngô <truongnq63.fbaelite@gmail.com>
2025-12-06 17:19:56 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Trường Ngô 🦆';
+export const SITE_TITLE = 'Trường Ngô 🦆 | An 2nd year International Business Management student at FTU';
 export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
